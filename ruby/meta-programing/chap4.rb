3e584bd15dea30acabdf7af38bde12ed395e6e62
ichigo-dev <ichigo@web-freework.com>
2021-12-20 10:56:59 +0900

    Update ruby

--- ruby/meta-programing/chap4.rb
+++ ruby/meta-programing/chap4.rb
@@ -67,6 +67,72 @@ end
 # "b"
 
 p "4"
-Test.hello_c
+#Test.hello_c
 # output
 # chap4.rb:18:in `hello_c': undefined local variable or method `c' for Test:Class (NameError)
+
+class MyClass
+  def initialize
+    @v = 1
+  end
+
+  def test
+    yield
+  end
+end
+
+@obj = MyClass.new
+
+@obj.instance_eval do
+  p self
+  p @v
+end
+# output
+# MyClass::---
+# 1
+
+@obj.test do
+  p self
+  p @v
+end
+# output
+# main
+# nil
+
+inc = Proc.new { |x| x+ 1 }
+p inc.call(2)
+# output
+# 3
+
+inc = lambda { |x| x + 2 }
+p inc.call(2)
+# output
+# 4
+
+inc = ->(x){ x + 2 }
+p inc.call(3)
+# output
+# 5
+
+def math(x, y)
+  yield(x, y)
+end
+
+def do_math(x, y, &operation)
+  math(x, y, &operation)
+end
+
+p do_math(2, 3) {|x, y| x * y}
+# output
+# 6
+
+def test
+  yield
+end
+
+def test_1
+  p "bb"
+end
+
+test { p "aa" }
+:test_1.to_proc.call
