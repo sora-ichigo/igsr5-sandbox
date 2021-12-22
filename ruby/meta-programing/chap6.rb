dc25c2aad3d5c6d783931f73a24de1cfa9646615
ichigo-dev <ichigo@web-freework.com>
2021-12-22 21:24:50 +0900

    Update ruby

---
+++ ruby/meta-programing/chap6.rb
@@ -0,0 +1,73 @@
+# user_input = "User input: #{gets()}"
+# p user_input
+# a = gets()
+# puts user_input.tainted?
+# puts a.tainted?
+
+# add_checked_attribute
+# 1
+
+# def add_checked_attribute(klass, attribute)
+#   eval "
+#     class #{klass}
+#       def #{attribute}=(value)
+#         raise 'Invalid attribute' unless value
+#         @#{attribute} = value
+#       end
+# 
+#       def #{attribute}
+#         @#{attribute}
+#       end
+#     end
+#   "
+# end
+
+# 2
+
+def add_checked_attribute(klass, attribute, &validation)
+  klass.class_eval do
+    define_method "#{attribute}" do
+      instance_variable_get "@#{attribute}"
+    end
+
+    define_method "#{attribute}=" do |value|
+      raise "Invalid attribute Error: #{value}" unless validation.call(value)
+      instance_variable_set("@#{attribute}", value)
+    end
+  end
+end
+
+add_checked_attribute(String, :my_attr) { |v| v > 18 }
+a = String.new
+
+p a.my_attr
+# a.my_attr= 11 → Invalid attribute Error
+a.my_attr = 20 # → OK
+p a.my_attr
+# a.my_attr = nil → Invalid attribute Error
+
+# 4
+
+class Class
+  def attr_checked(attribute, &validation)
+    define_method "#{attribute}" do
+      instance_variable_get "@#{attribute}"
+    end
+
+    define_method "#{attribute}=" do |value|
+      raise "Invalid attribute Error: #{value}" unless validation.call(value)
+      instance_variable_set("@#{attribute}", value)
+    end
+  end
+end
+
+class A
+  attr_checked :my_attr do |v|
+    v > 20
+  end
+end
+
+a = A.new
+p a.my_attr
+a.my_attr = 21
+p a.my_attr
