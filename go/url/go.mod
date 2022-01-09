e2936f1a44be6bced18f2259c7b213c08a8d79cc
ichigo-dev <ichigo@web-freework.com>
2022-01-10 01:41:16 +0900

    Update

--- go/url/go.mod
+++ go/url/go.mod
@@ -1,3 +1,3 @@
 module sandbox/url
 
-go 1.17
+go 1.17  
--- ruby/breack-return/main.rb
+++ ruby/breack-return/main.rb
@@ -1,19 +1,19 @@
-require "securerandom"
+require 'securerandom'
 
 def generate_url_key_1
-    loop do
-      url_key = "foo"
-      break url_key
-    end
+  loop do
+    url_key = 'foo'
+    break url_key
+  end
 end
 
 def generate_url_key_2
-    url_key = ""
-    loop do
-      url_key = "boo"
-      break
-    end
-    url_key
+  url_key = ''
+  loop do
+    url_key = 'boo'
+    break
+  end
+  url_key
 end
 
 p generate_url_key_1 # → foo
