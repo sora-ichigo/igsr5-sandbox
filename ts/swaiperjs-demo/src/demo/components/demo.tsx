5508179ff413cbd37bbb06401b92ecb91a0e49f3
igsr5 <i@gsr5.dev>
2022-01-18 02:07:45 +0900

    Update swaiper demo

--- ts/swaiperjs-demo/src/demo/components/demo.tsx
+++ ts/swaiperjs-demo/src/demo/components/demo.tsx
@@ -1,6 +1,16 @@
 import React, { useEffect, useState } from "react";
+import Link from "next/link";
+import styled from "styled-components";
 import type CoreSwiper from "swiper";
 import { Swiper, SwiperSlide } from "swiper/react";
+
+const pages = ["1", "2", "3", "4", "5"];
+
+type CustomSwiperSlideProps = {
+  isActive: boolean;
+  isNext: boolean;
+  isPrev: boolean;
+};
 
 export const Demo: React.FC = () => {
   const [swiper, setSwiper] = useState<CoreSwiper | undefined>(undefined);
@@ -8,49 +18,85 @@ export const Demo: React.FC = () => {
   const slidePrev = () => swiper?.slidePrev();
   const slideNext = () => swiper?.slideNext();
 
+  const CustomSwiperSlide: React.FC<CustomSwiperSlideProps> = ({
+    children,
+    isActive,
+    isNext,
+    isPrev,
+  }) => (
+    // TODO: click 連打でバグるから一定間隔イベントを無効化する
+    // 自分のポジションを isNext, isPrev で取得しないと言う手もある
+    <StyledDiv>
+      {isActive ? (
+        <>{children}</>
+      ) : isNext ? (
+        <>
+          <OverrayDiv onClick={() => slideNext()} />
+          {children}
+        </>
+      ) : isPrev ? (
+        <>
+          <OverrayDiv onClick={() => slidePrev()} />
+          {children}
+        </>
+      ) : null}
+    </StyledDiv>
+  );
+
   return (
     <>
-      <button onClick={() => slidePrev()}>prev</button>
-      <button onClick={() => slideNext()}>next</button>
-
       <Swiper
         slidesPerView={"auto"}
         spaceBetween={10}
-        slidesPerGroup={1}
         loop={true}
-        loopFillGroupWithBlank={true}
         centeredSlides={true}
         onInit={(swiper) => setSwiper(swiper)}
         onSlideChange={(swiper) => {
           console.log("slide change. to", swiper.activeIndex);
         }}
-        pagination={{
-          clickable: true,
-        }}
-        navigation={true}
         className="mySwiper"
       >
-        <SwiperSlide
-          style={{ width: "80%", height: "500px", background: "#f0f0f0" }}
-        >
-          Slide 1
+        <SwiperSlide>
+          <></>
         </SwiperSlide>
-        <SwiperSlide
-          style={{ width: "80%", height: "500px", background: "#f0f0f0" }}
-        >
-          Slide 2
-        </SwiperSlide>
-        <SwiperSlide
-          style={{ width: "80%", height: "500px", background: "#f0f0f0" }}
-        >
-          Slide 3
-        </SwiperSlide>
-        <SwiperSlide
-          style={{ width: "80%", height: "500px", background: "#f0f0f0" }}
-        >
-          Slide 4
-        </SwiperSlide>
+      </Swiper>
+
+      <Swiper
+        slidesPerView={"auto"}
+        spaceBetween={10}
+        loop={true}
+        centeredSlides={true}
+        onInit={(swiper) => setSwiper(swiper)}
+        onSlideChange={(swiper) => {
+          console.log("slide change. to", swiper.activeIndex);
+        }}
+        className="mySwiper"
+      >
+        {pages.map((v) => (
+          <SwiperSlide style={{ width: "80%" }} key={v}>
+            {(props) => (
+              <CustomSwiperSlide {...props}>
+                <Link href="/">
+                  <a>slide{v}</a>
+                </Link>
+              </CustomSwiperSlide>
+            )}
+          </SwiperSlide>
+        ))}
       </Swiper>
     </>
   );
 };
+
+const StyledDiv = styled.div`
+  height: 500px;
+`;
+
+const OverrayDiv = styled.div`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  cursor: pointer;
+  background-color: #ffffff9d;
+  z-index: 99999;
+`;
--- ts/swaiperjs-demo/src/pages/_app.tsx
+++ ts/swaiperjs-demo/src/pages/_app.tsx
@@ -1,6 +1,9 @@
 import React from "react";
-import SwiperCore, { Pagination, Navigation } from "swiper";
 import "swiper/css";
+import "swiper/css/navigation";
+import SwiperCore, { Pagination, Navigation } from "swiper";
+
+SwiperCore.use([Pagination, Navigation]);
 
 const App = ({ Component, pageProps }: any) => {
   return <Component {...pageProps} />;
