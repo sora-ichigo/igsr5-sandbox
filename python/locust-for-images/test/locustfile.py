53d7d9f8f0329c0060b75114257c8fb12ee5b0c0
igsr5 <i@gsr5.dev>
2022-01-18 13:53:10 +0900

    Update

--- python/locust-for-images/test/locustfile.py
+++ python/locust-for-images/test/locustfile.py
@@ -3,4 +3,6 @@ from locust import HttpUser, task
 class GetImage(HttpUser):
     @task
     def get_image(self):
+        # self.client.get("/PwwkcCA")
         self.client.get("/qwksUBo")
+        self.client.get("/ping")
