a4862c3b002804e1bdb8e5b44540d330156fe454
igsr5 <i@gsr5.dev>
2022-01-20 22:31:42 +0900

    Update

--- python/locust-for-images/test/locustfile.py
+++ python/locust-for-images/test/locustfile.py
@@ -4,6 +4,7 @@ class GetImage(HttpUser):
     @task
     def get_image(self):
         # self.client.get("/PwwkcCA")
-        self.client.get("/qwksUBo?width=600&height=350&format=png")
+        # self.client.get("/qwksUBo?width=600&height=350&format=png")
+        self.client.get("/h7d2yZ3?width=1136&format=webp")
         # self.client.get("/gMJFFpz?width=600&height=350&format=png")
         # self.client.get("/ping")
--- ruby/image-url-test/main.rb
+++ ruby/image-url-test/main.rb
@@ -3,4 +3,12 @@ def url_of_images(*args)
 end
 
 url_of_images("640x")
+# → ["640x"]
 url_of_images("640x300#")
+# → ["640x300#"]
+
+def url_of_images_1(w: nil, h: nil)
+  p w, h
+end
+url_of_images_1(**{w: 100, h:200})
+
