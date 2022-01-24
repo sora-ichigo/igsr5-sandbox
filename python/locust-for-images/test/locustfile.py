caddcea1bd10fc7d97f5a3c52260139e2f7e54be
igsr5 <i@gsr5.dev>
2022-01-24 13:47:26 +0900

    Update

--- python/locust-for-images/test/locustfile.py
+++ python/locust-for-images/test/locustfile.py
@@ -5,6 +5,13 @@ class GetImage(HttpUser):
     def get_image(self):
         # self.client.get("/PwwkcCA")
         # self.client.get("/qwksUBo?width=600&height=350&format=png")
-        self.client.get("/h7d2yZ3?width=1136&format=webp")
+        # self.client.get("/h7d2yZ3?width=1136&format=webp")
         # self.client.get("/gMJFFpz?width=600&height=350&format=png")
         # self.client.get("/ping")
+
+        # 274 KB
+        # self.client.get("/rkB8ePL?width=600&height=300")
+        # 1,200 KB
+        # self.client.get("/LXPdsTx?width=900&height=500&format=png")
+        # 3,359 KB
+        self.client.get("/aKnmkdN?width=900&height=500&format=png")
