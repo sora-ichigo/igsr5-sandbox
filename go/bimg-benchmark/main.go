038f2e1640dca7b7548278dcd7227fc46726993d
igsr5 <i@gsr5.dev>
2022-02-01 15:31:57 +0900

    Update

--- go/bimg-benchmark/main.go
+++ go/bimg-benchmark/main.go
@@ -18,18 +18,47 @@ func main() {
 
 }
 
-func convert(filename string) {
+// func convert(b *testing.B, filename string) {
+// 	buffer, err := bimg.Read(filename)
+// 	if err != nil {
+// 		b.Fatalf("failed to Read file. err: %v", err)
+// 	}
+//
+// 	newImage, err := bimg.NewImage(buffer).Convert(bimg.WEBP)
+// 	if err != nil {
+// 		b.Fatalf("failed to convert. err: %v", err)
+// 	}
+//
+// 	if bimg.NewImage(newImage).Type() == "png" {
+// 		b.Fatalf("bad convert.\nwant: %s\ngot: %s", "webp", bimg.NewImage(newImage).Type())
+// 	}
+// }
+
+func extract(filename string) {
 	buffer, err := bimg.Read(filename)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
 	}
 
-	newImage, err := bimg.NewImage(buffer).Convert(bimg.WEBP)
+	bimg.NewImage(buffer).Extract(500, 500, 30, 30)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
 	}
+}
 
-	if bimg.NewImage(newImage).Type() == "png" {
-		fmt.Fprintln(os.Stderr, "The image was converted into png")
+func crop(filename string) {
+	buffer, err := bimg.Read(filename)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+	}
+	options := bimg.Options{
+		Width:   500,
+		Height:  500,
+		Crop:    true,
+		Gravity: bimg.GravityCentre,
+	}
+	bimg.NewImage(buffer).Process(options)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
 	}
 }
--- go/bimg-benchmark/main_test.go
+++ go/bimg-benchmark/main_test.go
@@ -1,9 +1,61 @@
 package main
 
-import "testing"
+import (
+	"testing"
 
-// small image
-func BenchmarkConvertWithSmallSize(b *testing.B) { convert("./small.png") }
+	"github.com/h2non/bimg"
+)
 
-// large image
-func BenchmarkConvertWithLargeSize(b *testing.B) { convert("./large.png") }
+// target function. https://github.com/h2non/bimg#convert
+func convert(b *testing.B, filename string) {
+	b.Helper()
+
+	buffer, err := bimg.Read(filename)
+	if err != nil {
+		b.Fatalf("failed to read file. err: %v", err)
+	}
+
+	src := bimg.NewImage(buffer)
+
+	src.Convert(bimg.WEBP)
+}
+
+func resizeAndConvert(b *testing.B, filename string) {
+	b.Helper()
+
+	buffer, err := bimg.Read(filename)
+	if err != nil {
+		b.Fatalf("failed to read file. err: %v", err)
+	}
+
+	src := bimg.NewImage(buffer)
+
+	src.Resize(900, 500)
+	src.Convert(bimg.WEBP)
+}
+
+// benchmark of convert()
+
+// 3MB, 900x500px
+func BenchmarkConvertWithSmallSize(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		resizeAndConvert(b, "./small.png")
+	}
+}
+
+// 3MB, 6000x4000px
+func BenchmarkConvertWithLargeSize(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		resizeAndConvert(b, "./large.png")
+	}
+}
+
+// Extract
+func BenchmarkExtract(b *testing.B) {
+	extract("./large.png")
+}
+
+// Crop
+func BenchmarkCrop(b *testing.B) {
+	crop("./large.png")
+}
