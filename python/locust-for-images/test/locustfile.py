4fc5f656785854a1e3c7552086fe286965bea043
igsr5 <i@gsr5.dev>
2022-02-02 19:58:11 +0900

    Update

--- python/locust-for-images/test/locustfile.py
+++ python/locust-for-images/test/locustfile.py
@@ -4,5 +4,5 @@ class GetImage(HttpUser):
     @task
     def get_image(self):
         # self.client.get("/LXPdsTx?width=900&height=500&format=png")
-        self.client.get("/LXPdsTx?width=1000&height=600")
+        self.client.get("/i/LXPdsTx?width=1000&height=600")
         # self.client.get("/LXPdsTx?format=png")
