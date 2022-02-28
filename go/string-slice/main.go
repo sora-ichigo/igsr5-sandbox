6bbb8429b8065322e2a7b99de524fe4b9350df9e
igsr5 <i@gsr5.dev>
2022-02-28 17:08:20 +0900

    Update

--- go/string-slice/main.go
+++ go/string-slice/main.go
@@ -2,21 +2,46 @@ package main
 
 import (
 	"fmt"
-	"regexp"
+	"sort"
+	"strconv"
 	"strings"
 )
 
+type Data struct {
+	Type string
+	Q    float64
+}
+
+var str = "application/xhtml+xml,application/xml;q=0.9,image/avif,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9"
+
 func main() {
-	str := "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9"
+	arr := strings.Split(str, ",")
+	data := make([]Data, len(arr))
 
-	arr := []string{}
+	for i, aaa := range arr {
+		aa := strings.Split(aaa, ";")
 
-	arr = strings.Split(str, ",")
-
-	for _, a := range arr {
-		f, _ := regexp.MatchString("(?)image/webp|image/apng", a)
-		fmt.Println(f)
+		mimeType := strings.Trim(aa[0], " ")
+		q := getQFromAcceptString(aa)
+		data[i] = Data{Type: mimeType, Q: q}
 	}
 
-	fmt.Println(arr)
+	sort.SliceStable(data, func(i, j int) bool { return data[i].Q > data[j].Q })
+
+	fmt.Printf("%#v", data)
+}
+
+func getQFromAcceptString(arr []string) float64 {
+	for _, a := range arr {
+		if strings.HasPrefix(a, "q=") {
+			q, err := strconv.ParseFloat(a[2:], 64)
+			if err != nil {
+				return 1.0
+			}
+
+			return q
+		}
+	}
+
+	return 1.0
 }
