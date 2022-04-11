76e1410c44b68210144549d518e0cf8f4604b0dc
igsr5 <i@gsr5.dev>
2022-04-11 22:33:41 +0900

    Update c error_at

--- c/9cc/main.c
+++ c/9cc/main.c
@@ -31,6 +31,22 @@ void error(char *fmt, ...) {
   exit(1);
 }
 
+// 入力プログラム
+char *user_input;
+
+// エラー箇所を報告する
+void error_at(char *loc, char *fmt, ...) {
+  va_list ap;
+  va_start(ap, fmt);
+
+  int pos = loc - user_input;
+  fprintf(stderr, "%s\n", user_input);
+  fprintf(stderr, "%*s", pos, " "); // pos個の空白を出力
+  fprintf(stderr, "^ ");
+  vfprintf(stderr, fmt, ap);
+  fprintf(stderr, "\n");
+  exit(1);
+}
 
 // 新しいトークンを作成してcurに繋げる
 Token *new_token(TokenKind kind, Token *cur, char *str) {
