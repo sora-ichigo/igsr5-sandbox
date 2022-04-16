28e6d94902afb3640ffb9a90435e4335fae4b5c2
igsr5 <i@gsr5.dev>
2022-04-16 11:33:23 +0900

    Update c

--- c/9cc/main.c
+++ c/9cc/main.c
@@ -112,6 +112,80 @@ void expect(char op) {
   token = token->next;
 }
 
+// 抽象構文木のノードの種類
+typedef enum {
+  ND_ADD, // +
+  ND_SUB, // -
+  ND_MUL, // *
+  ND_DIV, // /
+  ND_NUM, // 整数
+} NodeKind;
+
+typedef struct Node Node;
+
+Node *expr();
+
+// 抽象構文木のノードの型
+struct Node {
+  NodeKind kind; // ノードの型
+  Node *lhs;     // 左辺
+  Node *rhs;     // 右辺
+  int val;       // kindがND_NUMの場合のみ使う
+};
+
+Node *new_node(NodeKind kind, Node *lhs, Node *rhs) {
+  Node *node = calloc(1, sizeof(Node));
+  node->kind = kind;
+  node->lhs = lhs;
+  node->rhs = rhs;
+  return node;
+}
+
+Node *new_node_num(int val) {
+  Node *node = calloc(1, sizeof(Node));
+  node->kind = ND_NUM;
+  node->val = val;
+  return node;
+}
+
+Node *primary() {
+  // 次のトークンが"("なら、"(" expr ")"のはず
+  if (consume('(')) {
+    Node *node = expr();
+    expect(')');
+    return node;
+  }
+
+  // そうでなければ数値のはず
+  return new_node_num(expect_number());
+}
+
+Node *mul() {
+  Node *node = primary();
+
+  for (;;) {
+    if (consume('*'))
+      node = new_node(ND_MUL, node, primary());
+    else if (consume('/'))
+      node = new_node(ND_DIV, node, primary());
+    else
+      return node;
+  }
+}
+
+Node *expr() {
+  Node *node = mul();
+
+  for (;;) {
+    if (consume('+'))
+      node = new_node(ND_ADD, node, mul());
+    else if (consume('-'))
+      node = new_node(ND_SUB, node, mul());
+    else
+      return node;
+  }
+}
+
 int main(int argc, char **argv) {
   if (argc != 2) {
     error("bad attributes. expect count is 2");
@@ -119,27 +193,29 @@ int main(int argc, char **argv) {
 
   user_input = argv[1];
 
-  token = tokenize(argv[1]);
+  Node node = *expr();
 
-  printf(".intel_syntax noprefix\n");
-  printf(".globl main\n");
-  printf("main:\n");
+  // printf("%d", node->kind);
+  // token = tokenize(argv[1]);
 
-  printf("  mov rax, %d\n", expect_number());
+  // printf(".intel_syntax noprefix\n");
+  // printf(".globl main\n");
+  // printf("main:\n");
 
-  while (!at_eof()) {
-    if (consume('+')) {
-      printf("  add rax, %d\n", expect_number());
-      continue;
-    }
+  // printf("  mov rax, %d\n", expect_number());
 
-    expect('-');
-    printf("  sub rax, %d\n", expect_number());
-  }
+  // while (!at_eof()) {
+  //   if (consume('+')) {
+  //     printf("  add rax, %d\n", expect_number());
+  //     continue;
+  //   }
+
+  //   expect('-');
+  //   printf("  sub rax, %d\n", expect_number());
+  // }
 
   
   printf("  ret\n");
   return 0;
 }
-
 
