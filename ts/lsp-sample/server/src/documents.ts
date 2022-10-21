8576c07d6c0542f30aa3eed9f8d14f63d79ae22e
igsr5 <igsr5.dev@gmail.com>
2022-10-21 23:56:41 +0900

    Implement basic tokenize

--- ts/lsp-sample/server/src/documents.ts
+++ ts/lsp-sample/server/src/documents.ts
@@ -1,10 +1,96 @@
 import { TextDocument } from 'vscode-languageserver-textdocument';
-import { Connection, DiagnosticSeverity, TextDocumentChangeEvent, TextDocuments } from 'vscode-languageserver/node';
-import { compile, CompileFiles } from './compile';
+import { TextDocumentChangeEvent, TextDocuments } from 'vscode-languageserver/node';
 import { getConnection } from './connections';
 import { Diagnostics } from './diagnostics';
 
-let buffers: CompileFiles = new Map();
+// eslint-disable-next-line @typescript-eslint/ban-types
+type CompileFiles = Map<string, Object>;
+
+const compile = (uri: string, src: string) => {
+	diagnostics.clear();
+	const tokens = tokenize(uri, src);
+
+	buffers.set(uri, tokens);
+	return buffers;
+};
+
+// eslint-disable-next-line @typescript-eslint/ban-types
+const tokenize = (uri: string, src: string): Object[] => {
+	let i = 0;
+	let line = 0;
+	let character = 0;
+	// eslint-disable-next-line @typescript-eslint/ban-types
+	const tokens: Object[] = [];
+
+	const nextChar = () => {
+		if (i >= src.length) return;
+		if (src[i] === "\n") {
+			++i;
+			++line;
+			character = 0;
+		} else {
+			++i;
+			++character;
+		}
+	};
+
+	while (true) {
+		// skip leading whitespaces
+		while (true) {
+			if (i >= src.length) return tokens;
+			if (" \n\t\r".indexOf(src[i]) === -1) break;
+			nextChar();
+		}
+
+		const start: { line: number, character: number } = { line, character };
+
+		let text;
+		let kind;
+
+		if (src[i] === "(") {
+			text = "(";
+			kind = "(";
+			nextChar();
+		} else if (src[i] === ")") {
+			text = ")";
+			kind = ")";
+			nextChar();
+		} else if (src[i] === ";") {
+			const begin = i;
+			while (true) {
+				if (i >= src.length) break;
+				if (src[i] === "\n") break;
+				nextChar();
+			}
+			text = src.substring(begin, i);
+			kind = "comment";
+		} else {
+			const begin = i;
+			while (true) {
+				if (i >= src.length) break;
+				if (" \n\t\r();".indexOf(src[i]) !== -1) break;
+				nextChar();
+			}
+
+			text = src.substring(begin, i);
+
+			if (!isNaN(Number(text))) {
+				kind = "number";
+			} else {
+				kind = "variable";
+			}
+		}
+
+		const end: { line: number, character: number } = { line, character };
+		const location = { uri, range: { start, end } };
+		tokens.push({ text, kind, location });
+	}
+
+	return tokens;
+};
+
+
+const buffers: CompileFiles = new Map();
 const diagnostics: Diagnostics = new Map();
 
 export class DocumentManager {
@@ -12,23 +98,27 @@ export class DocumentManager {
 
 	constructor() {
 		this.documents.onDidChangeContent(this.onDidChangeContent);
+		this.documents.onDidOpen(this.onDidOpen);
 	}
 
 	private onDidChangeContent(change: TextDocumentChangeEvent<TextDocument>) {
-		buffers = compile(buffers, change.document.uri, change.document.getText());
+		const uri = change.document.uri;
+		const text = change.document.getText();
+		// if (text.length !== 0) {
+		compile(uri, text);
+		// }
+		console.log(buffers);
+	}
 
-		diagnostics.set(change.document.uri, [{
-			severity: DiagnosticSeverity.Information,
-			range: {
-				start: { line: 0, character: 0 },
-				end: { line: 0, character: 10 }
-			},
-			message: "diagnostic message 1"
-		}]);
+	private onDidOpen(msg: TextDocumentChangeEvent<TextDocument>) {
+		const uri = msg.document.uri;
+		const text = msg.document.getText();
+		compile(uri, text);
 
 		const connection = getConnection();
-		diagnostics.forEach((diagnostics, uri) => {
-			connection.sendDiagnostics({ uri: uri, diagnostics: diagnostics });
-		});
+		const currentDiagnostics = diagnostics.get(uri);
+		if (currentDiagnostics) {
+			connection.sendDiagnostics({ uri, diagnostics: currentDiagnostics });
+		}
 	}
 }
