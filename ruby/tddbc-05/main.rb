e133945c65092559e125d9f2e638caeb5c0599fc
igsr5 <igsr5.dev@gmail.com>
2022-12-22 19:19:14 +0900

    run F
    Failures:
    
      1) Main should return '(4,7)'
         Failure/Error: expect(p4_7).to be_nil
    
           expected: nil
                got: #<GridPoint:0x00000001048a8270 @x=4, @y=7>
         # ./spec/main_spec.rb:6:in `block (2 levels) in <top (required)>'
    
    Finished in 0.00608 seconds (files took 0.04317 seconds to load)
    1 example, 1 failure
    
    Failed examples:
    
    rspec ./spec/main_spec.rb:4 # Main should return '(4,7)'

--- ruby/tddbc-05/main.rb
+++ ruby/tddbc-05/main.rb
@@ -1,5 +1,10 @@
 # from. http://devtesting.jp/tddbc/?TDDBC%E4%BB%99%E5%8F%B005%2F%E8%AA%B2%E9%A1%8C
 
-def grid_point
-  "(4,7)"
+class GridPoint
+  attr_accessor :x, :y
+
+  def initialize(x, y)
+    @x = x
+    @y = y
+  end
 end
--- ruby/tddbc-05/spec/main_spec.rb
+++ ruby/tddbc-05/spec/main_spec.rb
@@ -2,6 +2,7 @@ require_relative "../main.rb"
 
 describe "Main" do
   it "should return '(4,7)'" do
-    expect(grid_point).to eq("(4,7)")
+    p4_7 = GridPoint.new(4, 7)
+    expect(p4_7).to be_nil
   end
 end
