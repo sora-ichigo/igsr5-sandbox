9c23d2a63c6f18642993511eee21a289558a76d2
igsr5 <igsr5.dev@gmail.com>
2022-12-22 19:29:28 +0900

    run .....
    Finished in 0.00162 seconds (files took 0.0425 seconds to load)
    5 examples, 0 failures

--- ruby/tddbc-05/spec/main_spec.rb
+++ ruby/tddbc-05/spec/main_spec.rb
@@ -22,13 +22,13 @@ describe "GridPoint" do
   describe "#has_same_coordinates_with" do
     context "when two GridPoint object have same coordinates" do
       it "should return true" do
-        expect(obj.has_same_coordinates_with(GridPoint.new(x, y))).to eq true
+        expect(obj.same_coordinates_with?(GridPoint.new(x, y))).to eq true
       end
     end
 
     context "when two GridPoint object have different coordinates" do
       it "should return true" do
-        expect(obj.has_same_coordinates_with(GridPoint.new(x+1, y+1))).to eq false
+        expect(obj.same_coordinates_with?(GridPoint.new(x+1, y+1))).to eq false
       end
     end
   end
