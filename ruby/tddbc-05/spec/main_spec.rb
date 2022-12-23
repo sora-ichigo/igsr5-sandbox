a03b42d99a7e12b406922c12ea069deb90f2bdfa
igsr5 <igsr5.dev@gmail.com>
2022-12-23 12:23:39 +0900

    run ....................................
    Finished in 0.00471 seconds (files took 0.04622 seconds to load)
    36 examples, 0 failures

--- ruby/tddbc-05/spec/main_spec.rb
+++ ruby/tddbc-05/spec/main_spec.rb
@@ -117,7 +117,7 @@ describe "GridPointSet" do
     end
   end
 
-  context "when grid_point_set has two grid_points" do
+  context "when grid_point_set has three grid_points" do
     let(:grid_point1) { GridPoint.new(1, 2) }
     let(:grid_point2) { GridPoint.new(3, 6) }
     let(:grid_point3) { GridPoint.new(9, 18) }
@@ -213,6 +213,91 @@ describe "GridPointSet" do
     describe "#count" do
       it "should return grid_points count" do
         expect(grid_point_set.count).to eq 3
+      end
+    end
+  end
+
+  context "when grid_point_set has four grid_points" do
+    let(:grid_point1) { GridPoint.new(1, 2) }
+    let(:grid_point2) { GridPoint.new(3, 6) }
+    let(:grid_point3) { GridPoint.new(9, 18) }
+    let(:grid_point4) { GridPoint.new(27, 54) }
+    let(:grid_point_set) { GridPointSet.new(grid_point1, grid_point2, grid_point3, grid_point4) }
+
+    it do
+      expect(grid_point_set.grid_points).to eq [grid_point1, grid_point2, grid_point3, grid_point4]
+    end
+
+    describe "#contain?" do
+      context "when contains given grid_points contain" do
+        it "should return true" do
+          expect(grid_point_set.contain?(grid_point3)).to eq true
+        end
+      end
+      context "when doesn't contains given grid_points contain" do
+        it "should return false" do
+          expect(grid_point_set.contain?(GridPoint.new(10, 20))).to eq false
+        end
+      end
+    end
+
+    # 格子点集合に含まれる任意の格子点について、
+    # その格子点から隣り合っている格子点のみを経由して
+    # その他全ての格子点へ到達できる場合に限り、
+    # その格子点集合が連結しているものとします
+    describe "#connected?" do
+      context "when grid_point_set is connected" do
+        context "ver1" do
+          let(:grid_point1) { GridPoint.new(1, 2) }
+          let(:grid_point2) { GridPoint.new(2, 2) }
+          let(:grid_point3) { GridPoint.new(3, 2) }
+          let(:grid_point4) { GridPoint.new(4, 2) }
+
+          it "should return true" do
+            expect(grid_point_set.connected?).to eq true
+          end
+        end
+
+        context "ver2" do
+          let(:grid_point1) { GridPoint.new(1, 2) }
+          let(:grid_point2) { GridPoint.new(1, 3) }
+          let(:grid_point3) { GridPoint.new(1, 2) }
+          let(:grid_point4) { GridPoint.new(2, 2) }
+
+          it "should return true" do
+            expect(grid_point_set.connected?).to eq true
+          end
+        end
+      end
+
+      context "when grid_point_set is not connected" do
+        context "ver1" do
+          let(:grid_point1) { GridPoint.new(1, 2) }
+          let(:grid_point2) { GridPoint.new(2, 2) }
+          let(:grid_point3) { GridPoint.new(4, 2) }
+          let(:grid_point4) { GridPoint.new(5, 2) }
+
+          it "should return false" do
+            expect(grid_point_set.connected?).to eq false
+          end
+        end
+
+        context "ver2" do
+          let(:grid_point1) { GridPoint.new(1, 2) }
+          let(:grid_point2) { GridPoint.new(2, 2) }
+          let(:grid_point3) { GridPoint.new(3, 2) }
+          let(:grid_point4) { GridPoint.new(5, 2) }
+
+          it "should return false" do
+            expect(grid_point_set.connected?).to eq false
+          end
+        end
+      end
+    end
+
+    describe "#count" do
+      it "should return grid_points count" do
+        expect(grid_point_set.count).to eq 4
       end
     end
   end
