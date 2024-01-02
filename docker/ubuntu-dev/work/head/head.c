3c1a2e1e98a2641c753178c60e1e1d38538fdbf5
igsr5 <igsr5.dev@gmail.com>
2024-01-02 22:55:35 +0900

    Add command line options to specify number of lines to display in head.c

--- docker/ubuntu-dev/work/head/head.c
+++ docker/ubuntu-dev/work/head/head.c
@@ -1,40 +1,61 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define _GNU_SOURCE
+#include <getopt.h>
 
-static void do_head(FILE *f, long n);
+#define DEFAULT_N_LINES 10
+
+static struct option longopts[] = {
+    {"lines", required_argument, NULL, 'n'},
+    {"help", no_argument, NULL, 'h'},
+    {0, 0, 0, 0}};
+
+static void
+do_head(FILE *f, long n);
 
 int main(int argc, char **argv)
 {
-  if (argc > 2)
-  {
-    fprintf(stderr, "Usage: %s n [file file...]\n", argv[0]);
-    exit(1);
-  }
+  int opt;
+  int nlines = DEFAULT_N_LINES;
 
-  if (argc == 2)
+  while ((opt = getopt_long(argc, argv, "n:", longopts, NULL)) != -1)
   {
-    FILE *f;
-    f = fopen(argv[1], "r");
-    if (!f)
+    switch (opt)
     {
-      perror(argv[1]);
+    case 'n':
+      nlines = atoi(optarg);
+      break;
+    case 'h':
+      fprintf(stdout, "Usage: %s [-n LINES] [FILE ...]\n", argv[0]);
+      exit(0);
+    case '?':
+      fprintf(stderr, "Usage: %s [-n LINES] [FILE ...]\n", argv[0]);
       exit(1);
     }
+  };
 
-    do_head(f, 10);
-    if (fclose(f) < 0)
+  if (optind == argc)
+  {
+    do_head(stdin, nlines);
+  }
+  else
+  {
+    int i;
+    for (i = optind; i < argc; i++)
     {
-      perror(argv[1]);
-      exit(1);
+      FILE *f;
+      f = fopen(argv[i], "r");
+      if (!f)
+      {
+        perror(argv[1]);
+        exit(1);
+      };
+
+      do_head(f, nlines);
     }
   }
 
-  long n;
-  n = atol(argv[1]);
-
-  do_head(stdin, n);
-
-  return 0;
+  exit(0);
 }
 
 static void do_head(FILE *f, long n)
