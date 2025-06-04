9e4a0f7546fdee8ac32feb61331ab18d722bd5fb
Sora ichigo <igsr5.dev@gmail.com>
2025-06-04 16:03:23 +0900

    bedrock_model

--- python/strands-sdk-python/agent.py
+++ python/strands-sdk-python/agent.py
@@ -1,4 +1,14 @@
 from strands import Agent
 from strands_tools import calculator
-agent = Agent(tools=[calculator])
+from strands.models.bedrock import BedrockModel
+
+bedrock_model = BedrockModel(
+    model_id="arn:aws:bedrock:ap-northeast-1:247679429177:inference-profile/apac.anthropic.claude-sonnet-4-20250514-v1:0",
+    region_name='ap-northeast-1',
+)
+
+
+agent = Agent(tools=[calculator], model=bedrock_model)
+print(agent.model.config)
+
 agent("What is the square root of 1764")
