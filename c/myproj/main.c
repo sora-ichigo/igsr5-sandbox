f922842cb98202850cfceb6266f20701ab069ae0
Sora ichigo <igsr5.dev@gmail.com>
2025-11-24 21:12:30 +0900

    feat: 基本的な数学関数を持つCプロジェクトを追加
    add関数とmul関数を実装し、Makefileでビルド管理を行うシンプルなCプロジェクトを作成。
    コンパイル済みファイルを.gitignoreに追加。

---
+++ c/myproj/main.c
@@ -0,0 +1,12 @@
+#include "math.h"
+#include <stdio.h>
+
+int main(void) {
+  int a = add(3, 4);
+  int b = mul(2, 5);
+
+  printf("a = %d\n", a);
+  printf("b = %d\n", b);
+
+  return 0;
+}
---
+++ c/myproj/math.h
@@ -0,0 +1,7 @@
+#ifndef MY_MATH_H
+#define MY_MATH_H
+
+int add(int a, int b);
+int mul(int a, int b);
+
+#endif // !MY_MATH_H
